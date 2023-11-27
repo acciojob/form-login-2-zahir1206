c45595f77305757764a1c7649166a7e30ae56c44
Md Zahirul Haque <116150226+zahir1206@users.noreply.github.com>
2023-11-28 00:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const form=document.getElementById("form");
+const onSubmitForm(event){
+	const data={
+		First Name:event.target.First Name.value;
+		Last Name:event.target.Last Name.value;
+		Phone Number:event.target.Phone Number.value;
+		Email ID:event.target.Email ID.value;
+	}
+	alert(data);
+}
+form.addEventListener("submit",onSubmitForm);
+alert(form);
